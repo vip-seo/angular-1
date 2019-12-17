032ab90410e299514d901a5148955a855fbb6b3a
vip-seo <csat.vip@gmail.com>
2019-12-17 18:48:08 +0200

    some

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public chapterDesc: string = 'Same description';
-  constructor() { }
+  constructor() { 
+    console.log('constructor');
+  }
 
-  ngOnInit() {
+  ngOnInit()  {
+    console.log('ngOnInit');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 public getDesc(): string {
   return this.chapterDesc;
